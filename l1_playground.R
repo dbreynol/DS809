22b22ede973ee6e03dd7b5241ac41b0300398e85
dbreynol <david.reynolds@unh.edu>
2025-01-23 15:10:48 -0500

    lecture 1

---
+++ l1_playground.R
@@ -0,0 +1,4 @@
+library(fma)
+
+# frequency(elec)
+plot ( sin (2 * pi * time(elec) ) )
--- pacfprobs.R
+++ pacfprobs.R
@@ -29,3 +29,31 @@ coefficients ( lm(Price ~ Mileage + Age, car) )
 tv = read.csv("https://cnow.apps.ng.cengage.com/ilrn/books/ca3mbs08h/showtime_r.csv")
 m0 = lm(Revenue ~ TV + Newspaper, tv)
 predict(m0, newdata = data.frame(TV = 3.2, Newspaper = 2.7), interval = "confidence")
+
+
+
+
+
+
+library(bsts)
+
+y = Nile
+ss = list()
+ss = AddLocalLevel(ss, y)
+llmodel <- bsts(y,
+                 state.specification = ss,
+                 niter = 1000)
+
+PlotBstsComponents(llmodel)
+
+hist(llmodel$sigma.obs)
+median(llmodel$sigma.obs^2) # 15242
+hist(llmodel$sigma.level)
+median(llmodel$sigma.level^2) # 1477.533
+
+# local level version
+mll = StructTS(y, type = "level")
+str(mll)
+plot(mll$fitted)
+
+mll$coef
