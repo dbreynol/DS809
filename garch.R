afd3a1fbbd08bcc16419815d37deb573564d5c14
dbreynol <david.reynolds@unh.edu>
2025-02-12 13:21:49 -0500

    midreview

--- garch.R
+++ garch.R
@@ -9,3 +9,37 @@ predict(md)
 
 md1 = garchFit(formula =  ~ arma(1,0) + garch(1,1), data = Nile)
 summary(md1)
+
+# arch(1)
+# y_t = sigma_t * w_t
+# sigma_t^2 = alpha_0 + alpha_1 * y_{t-1}^2
+set.seed(1)
+alpha_0 = .1
+alpha_1 = .4
+n = 1000
+r_tm1 = 0
+returns = array( dim = n )
+
+for(t in 1:n) {
+  r_t = rnorm(1, mean = 0, sd = sqrt(alpha_0 + alpha_1 * r_tm1^2 ))
+  returns[t] = r_t
+  r_tm1 = r_t
+}
+
+acf(returns)
+(acf(returns ^ 2))
+
+mv1 = fGarch::garchFit(data = returns, formula = ~garch(1,1))
+summary(mv1)
+plot(returns, type = "l")
+
+
+
+
+install.packages("devtools")
+devtools::install_github("five-dots/rpolygon.io")
+#POLYGON_KEY = "yDOlUmAgEZklJkhw_LBwFmL5_hYFnqoU"
+Sys.getenv("POLYGON_KEY")
+library(rpolygon.io)
+last_aapl <- rpolygon("/v1/last/stocks/AAPL")
+prev_aapl <- rpolygon("/v2/aggs/ticker/AAPL/prev", args = list(unadjusted = "true"))
--- l3.R
+++ l3.R
@@ -1,46 +1,81 @@
 library(forecast)
 library(tidyverse)
+library(tseries)
+library(fpp3)
+
+# 0. Find some transformation to make our time series stationary
+# 1. visualization: time series / acf / pacf .... candidate models
+# 2. Fit candidate models .... choose LogLik
+# 3. Check the residuals: want the residuals to resemble white noise
+#    centered 0; constant variance; rho(h) = 0; h > 1
+# 4. Forecast! 
+
+# first difference
+vec = c(1,3,7,7,10)
+diff(vec)
+
+# random walk
+# x_t = x_{t-1} + w_t
+# nabla(x_t) = x_{t - 1} + w_t - x_{t-1} = w_t
+
 
 # ARIMA(0,1,0)
 btc = read.csv("https://tinyurl.com/3epw5n4z") %>% 
-  mutate(timestamp = ymd(timestamp)) %>% 
-  mutate(lbtc = log(btc)) %>% 
-  mutate(lbtc_1d = c(NA, diff(lbtc)))
+  mutate(timestamp = ymd(timestamp))
+
 
 plot(btc$btc, type = "l")
+plot(diff(log(btc$btc)), type = "l")
 plot(diff(btc$btc), type = "l")
-plot(diff(btc$lbtc), type = "l")
 
-tseries::adf.test(btc$lbtc_1d[-1])
+tseries::adf.test(diff(log(btc$btc)))
+# H_0: not stationary
+# H_A: stationary
 
-m0 = Arima(btc$lbtc, order = c(0,1,0))
-m1 = Arima(btc$lbtc, order = c(0,1,1))
+y = diff(log(btc$btc))
+acf(y)
 
-var(btc$lbtc_1d, na.rm = T)
+m0 = Arima( log(btc$btc) , order = c(0, 1, 1)) # Loglik = 4049.18
 forecast::checkresiduals(m0)
+
+m1 = auto.arima(log(btc$btc)) # loglik = 4051.79/ 
 forecast::checkresiduals(m1)
-acf(btc$lbtc_1d[-1], type = "covariance")[0:1]
-plot(forecast(m0, h = 100))
-
-m1 = auto.arima(btc$lbtc)
-
-m1 = Arima(btc$lbtc_1d[-1], order = c(0,0,0))
-plot(forecast(m1, h = 100))
-checkresiduals(m1)
+plot ( forecast::forecast(m1 , 100) ) 
 
 
-head(elec)
-plot(elec)
-plot((log(elec)))
-z12 = diff(log(elec))
-z122 = diff(z12, 12) 
-acf(z122, lag.max = 36) # MA1/ 
-acf(z122, type = "partial", lag.max = 48)
-ms1 = auto.arima(z122)
-ms0 = Arima(elec, order = c(0,1,1), seasonal = c(0,1,2))
+# SARIMA
+plot(fma::elec)
+frequency(elec)
+
+# 0. make this thing stationary
+# transformation: first difference ( lag 12 difference ( log elec) )
+
+y1 = diff( log(elec) )
+y2 = diff( y1 , 12 )
+plot(y2)
+tseries::adf.test(y2)
+
+# SARIMA(p, d, q)(P, D, Q)_12
+
+# SARIMA(0,1,1)(0, 1, 2)_{12}
+acf(y2, lag.max = 36)
+ms0 = Arima(log(elec), order = c(0, 1, 1), seasonal = c(0,1,2)) # loglik 1131.03
+checkresiduals(ms0)
+
+ms1 = auto.arima(log(elec)) # loglik 1134
 checkresiduals(ms1)
-tseries::adf.test(diff(log(elec),12))
-auto.arima(elec)
-mauto = auto.arima(log(elec))
-plot(forecast(mauto, h = 12))
+
+plot ( forecast(ms1, h = 24) )
+
+
+
+
+
+
+
+
+
+
+
+
 
